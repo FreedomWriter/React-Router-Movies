c5d22980448a119129d04c758077521248941065
FreedomWriter <natalienetworks@gmail.com>
2019-10-02 20:28:47 -0500

    mvp met

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,37 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies')
+        .get("http://localhost:5000/api/movies")
         .then(response => {
           setMovies(response.data);
         })
         .catch(error => {
-          console.error('Server Error', error);
+          console.error("Server Error", error);
         });
-    }
-    
+    };
+
     getMovies();
   }, []);
-  
+
   return (
-    <div className="movie-list">
+    <div key={props.id} className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link key={movie.id} to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
-}
+};
 
 function MovieDetails({ movie }) {
+  console.log(movie);
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
       <h2>{title}</h2>
+
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
