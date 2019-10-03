104c24557195aaf2b3f6699c838596781dee3fe3
FreedomWriter <natalienetworks@gmail.com>
2019-10-02 22:08:47 -0500

    refactoring done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import Movie from "./Movies/Movie";
 import MovieList from "./Movies/MovieList";
 import SavedList from "./Movies/SavedList";
+// import MovieCard from "./Movies/MovieCard";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -14,6 +15,7 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
+      {/* <MovieCard /> */}
       <Route exact path="/movies" component={MovieList} />
       <Route path="/movies/:id" component={Movie} />
     </div>
